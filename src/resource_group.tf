d7d7241ca134b9850b496852530c43b8d54b2dd8
ibera <ibera2010@gmail.com>
2022-08-01 20:07:21 +0200

    Remove rg

--- src/resource_group.tf
+++ src/resource_group.tf
@@ -2,7 +2,7 @@
 # RESOURCE GROUP
 #################################################################
 
-resource "azurerm_resource_group" "rg_project" {
-  name     = var.resource_group_name
-  location = var.resource_group_region
-}
+# resource "azurerm_resource_group" "rg_project" {
+#   name     = var.resource_group_name
+#   location = var.resource_group_region
+# }
